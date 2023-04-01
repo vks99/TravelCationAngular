60c5ee02722d29106018fde3e6b2a3200c2b25d4
SaradaChowdary <saradamaddipatla364@gmail.com>
2023-03-31 21:41:44 -0400

    changes

--- travel-client/src/app/app.module.ts
+++ travel-client/src/app/app.module.ts
@@ -23,11 +23,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
