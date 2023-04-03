6826041bf895a5f77d75f6ac3363761f91d7ca91
suzhanghu <n01572673@humber.ca>
2023-04-03 17:14:01 -0400

    signup

--- travel-client/src/app/app-routing.module.ts
+++ travel-client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactusComponent } from './contactus/contactus.component';
 import {PaymentComponent} from './payment/payment.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: 'contactus', component: ContactusComponent },
-  { path: 'payment', component: PaymentComponent }
+  { path: 'payment', component: PaymentComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
--- travel-client/src/app/signup/signup.component.ts
+++ travel-client/src/app/signup/signup.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupForm?: FormGroup;
+  errorMessage?: string;
 
+  constructor(private router: Router, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      user_name: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, Validators.required),
+      address: new FormControl(null, Validators.required)
+    });
+  }
+
+  onChange(event: any) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.signupForm?.get(name)?.setValue(value);
+  }
+
+  onSubmit() {
+    const { user_name, password, name, email, phone, address } = this.signupForm?.value;
+
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+	  address:address,
+	  phone:phone,
+    };
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    this.http.post<any>('http://localhost:8000/register', data, config)
+      .subscribe(
+        response => {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/Destination']);
+        },
+        error => {
+          this.errorMessage = error.message;
+        }
+      );
+  }
+
+  get user_name() { return this.signupForm?.get('user_name'); }
+  get password() { return this.signupForm?.get('password'); }
+  get name() { return this.signupForm?.get('name'); }
+  get email() { return this.signupForm?.get('email'); }
+  get phone() { return this.signupForm?.get('phone'); }
+  get address() { return this.signupForm?.get('address'); }
 }
